cd064a51535f9e98a41a020231b6f95c915af706
dnd_constuctive <dnd@constructive.io>
2024-08-05 14:35:25 +0200

    replace dialog with modal

--- src/webparts/technicalDocumentation/components/subscribe-options/SubscribeModal.tsx
+++ src/webparts/technicalDocumentation/components/subscribe-options/SubscribeModal.tsx
@@ -3,13 +3,75 @@ import Service from '../../service/Service';
 import { ListsData } from '../../models/ListData';
 import { UserEmail } from '../App';
 import { ProductOptionsProps } from './SubscribeButton';
-import { DefaultButton, Dialog, DialogContent, DialogFooter, DialogType } from 'office-ui-fabric-react';
+import { DefaultButton, IconButton, Modal } from 'office-ui-fabric-react';
 import { ComboBox, PrimaryButton } from '@fluentui/react';
+import { IButtonStyles } from '@fluentui/react/lib/Button';
 
-const dialogContentProps = {
-    type: DialogType.normal,
-    title: 'Subscribe on topics',
+import {
+    getTheme,
+    mergeStyleSets,
+    FontWeights,
+    IIconProps,
+} from '@fluentui/react';
+
+const theme = getTheme();
+const contentStyles = mergeStyleSets({
+    container: {
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        alignItems: 'stretch',
+    },
+    header: [
+        // eslint-disable-next-line deprecation/deprecation
+        theme.fonts.xLargePlus,
+        {
+            flex: '1 1 auto',
+            borderTop: `4px solid ${theme.palette.themePrimary}`,
+            color: theme.palette.neutralPrimary,
+            display: 'flex',
+            alignItems: 'center',
+            fontWeight: FontWeights.semibold,
+            padding: '12px 12px 14px 24px',
+        },
+    ],
+    heading: {
+        color: theme.palette.neutralPrimary,
+        fontWeight: FontWeights.semibold,
+        fontSize: 'inherit',
+        margin: '0',
+    },
+    body: {
+        flex: '4 4 auto',
+        padding: '0 24px 24px 24px',
+        overflowY: 'hidden',
+        selectors: {
+            p: { margin: '14px 0' },
+            'p:first-child': { marginTop: 0 },
+            'p:last-child': { marginBottom: 0 },
+        },
+    },
+    footer: {
+        flex: '4 4 auto',
+        padding: '0 24px 24px 24px',
+        overflowY: 'hidden',
+        textAlign: "right"
+    },
+});
+
+
+const iconButtonStyles: Partial<IButtonStyles> = {
+    root: {
+        color: theme.palette.neutralPrimary,
+        marginLeft: 'auto',
+        marginTop: '4px',
+        marginRight: '2px',
+    },
+    rootHovered: {
+        color: theme.palette.neutralDark,
+    },
 };
+
+const cancelIcon: IIconProps = { iconName: 'Cancel' };
 
 interface SubscribeModalProps {
     isOpen: boolean;
@@ -60,13 +122,26 @@ const SubscribeModal = ({ isOpen, productOptions, setIsOpen }: SubscribeModalPro
 
     return (
         <>
-            <Dialog
-                hidden={!isOpen}
+            <Modal
+                isOpen={isOpen}
                 onDismiss={() => setIsOpen(false)}
-                dialogContentProps={dialogContentProps}
+                isBlocking={false}
+                containerClassName={contentStyles.container}
             >
-                <DialogContent>
+                <div className={contentStyles.header}>
+                    <h2 className={contentStyles.heading}>
+                        Subscribe on products
+                    </h2>
+                    <IconButton
+                        styles={iconButtonStyles}
+                        iconProps={cancelIcon}
+                        ariaLabel="Close popup modal"
+                        onClick={() => setIsOpen(false)}
+                    />
+                </div>
+                <div className={contentStyles.body}>
                     <ComboBox
+                        style={{ minWidth: "700px" }}
                         label="Select Products:"
                         options={productOptions}
                         onChange={(e, v) => {
@@ -82,21 +157,22 @@ const SubscribeModal = ({ isOpen, productOptions, setIsOpen }: SubscribeModalPro
                         autoComplete="on"
                         calloutProps={{
                             onLayerMounted: () => {
-                              // Force the ComboBox options layer to be on top
-                              const layers = document.getElementsByClassName('ms-Layer');
-                              if (layers.length > 0) {
-                                const lastLayer:any = layers[layers.length - 1];
-                                lastLayer.style.zIndex = '1000';
-                              }
+                                // Force the ComboBox options layer to be on top
+                                const layers = document.getElementsByClassName('ms-Layer');
+                                if (layers.length > 0) {
+                                    const lastLayer: any = layers[layers.length - 1];
+                                    lastLayer.style.zIndex = '1000';
+                                }
                             }
-                          }}
+                        }}
                     />
-                </DialogContent>
-                <DialogFooter>
-                    <PrimaryButton onClick={save} text="Save" />
+
+                </div>
+                <div className={contentStyles.footer}>
+                    <PrimaryButton onClick={save} text="Save" style={{ marginRight: "10px" }} />
                     <DefaultButton onClick={() => setIsOpen(false)} text="Cancel" />
-                </DialogFooter>
-            </Dialog>
+                </div>
+            </Modal>
         </>
     )
 }
